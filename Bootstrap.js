3e3191ccc09d58c90b54a564f351aff96ba41ebf
Julian <julian_delisio@hotmail.com>
2023-01-28 23:10:49 -0300

    completado ejercicios bootstrap

--- Bootstrap.js
+++ Bootstrap.js
@@ -405,50 +405,369 @@ Finalmente, remueve la clase smaller-image de tu primer elemento img y reempláz
       <button class="btn btn-block btn-danger"><span class="fas fa-trash"> Delete</button>
     </div> */
 
-/* ------------------------------------------------------------- */
+/* Anida tus dos botones de radio dentro de un elemento <div class="row">. Luego, anida cada uno de ellos dentro de un elemento <div class="col-xs-6"> */
+
+/* <div class="row">
+      <div class="col-xs-6">
+        <label><input type="radio" name="indoor-outdoor"> Indoor</label>
+      </div>
+      <div class="col-xs-6">
+        <label><input type="radio" name="indoor-outdoor"> Outdoor</label>
+      </div>
+    </div> */
+
+/* Anida las tres casillas de verificación en un elemento <div class="row">. Luego, anida cada uno de ellos en un elemento <div class="col-xs-4"> */
+
+/*   <div class="row">
+      <div class="col-xs-4">
+        <label><input type="checkbox" name="personality"> Loving</label>
+      </div>
+      <div class="col-xs-4">
+        <label><input type="checkbox" name="personality"> Lazy</label>
+      </div>
+      <div class="col-xs-4">
+        <label><input type="checkbox" name="personality"> Crazy</label>
+      </div>
+    </div> */
+
+/* Estiliza entradas de texto como controles de formulario
+Puedes agregar el icono fa-paper-plane de Font Awesome agregando <i class="fa fa-paper-plane"></i> dentro de tu elemento button de envío.
+
+Dale a tu campo de entrada de texto la clase form-control. Dale a tu botón de envío de formulario las clases btn btn-primary. También asígnale a este botón el icono de Font Awesome fa-paper-plane.
+
+Todos los elementos textuales <input>, <textarea>, y <select> con la clase .form-control tienen un ancho de 100%. */
+
+/* <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet" type="text/css">
+<style>
+  h2 {
+    font-family: Lobster, Monospace;
+  }
+
+  .thick-green-border {
+    border-color: green;
+    border-width: 10px;
+    border-style: solid;
+    border-radius: 50%;
+  }
+
+</style>
+
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-xs-8">
+      <h2 class="text-primary text-center">CatPhotoApp</h2>
+    </div>
+    <div class="col-xs-4">
+      <a href="#"><img class="img-responsive thick-green-border" src="https://cdn.freecodecamp.org/curriculum/cat-photo-app/relaxing-cat.jpg" alt="A cute orange cat lying on its back."></a>
+    </div>
+  </div>
+  <img src="https://cdn.freecodecamp.org/curriculum/cat-photo-app/running-cats.jpg" class="img-responsive" alt="Three kittens running towards the camera.">
+  <div class="row">
+    <div class="col-xs-4">
+      <button class="btn btn-block btn-primary"><i class="fa fa-thumbs-up"></i> Like</button>
+    </div>
+    <div class="col-xs-4">
+      <button class="btn btn-block btn-info"><i class="fa fa-info-circle"></i> Info</button>
+    </div>
+    <div class="col-xs-4">
+      <button class="btn btn-block btn-danger"><i class="fa fa-trash"></i> Delete</button>
+    </div>
+  </div>
+  <p>Things cats <span class="text-danger">love:</span></p>
+  <ul>
+    <li>cat nip</li>
+    <li>laser pointers</li>
+    <li>lasagna</li>
+  </ul>
+  <p>Top 3 things cats hate:</p>
+  <ol>
+    <li>flea treatment</li>
+    <li>thunder</li>
+    <li>other cats</li>
+  </ol>
+  <form action="https://freecatphotoapp.com/submit-cat-photo">
+    <div class="row">
+      <div class="col-xs-6">
+        <label><input type="radio" name="indoor-outdoor"> Indoor</label>
+      </div>
+      <div class="col-xs-6">
+        <label><input type="radio" name="indoor-outdoor"> Outdoor</label>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-xs-4">
+        <label><input type="checkbox" name="personality"> Loving</label>
+      </div>
+      <div class="col-xs-4">
+        <label><input type="checkbox" name="personality"> Lazy</label>
+      </div>
+      <div class="col-xs-4">
+        <label><input type="checkbox" name="personality"> Crazy</label>
+      </div>
+    </div>
+    <input class="form-control "type="text" placeholder="cat photo URL" required>
+    <button class="btn btn-primary" type="submit"><i class=" fa fa-paper-plane"></i> Submit</button>
+  </form>
+</div> */
+
+/* Coloca juntos tu campo de texto input y tu button de envío dentro de un div con la clase row. Anida tu campo de texto input dentro de un div con la clase col-xs-7. Anida tu button de envío del formulario dentro de un div con la clase col-xs-5 */
+
+/*  <div class="row">
+      <div class="col-xs-7">
+        <input type="text" class="form-control" placeholder="cat photo URL" required>
+      </div>
+      <div class="col-xs-5">
+        <button type="submit" class="btn btn-primary"><i class="fa fa-paper-plane"></i> Submit</button>
+      </div>
+    </div> */
+
+/* ara empezar, crea un elemento h3, con el texto jQuery Playground.
+
+Colorea tu elemento h3 con la clase text-primary y céntralo con la clase text-center de Bootstrap. */
+
+/* <h3 class="text-primary text-center">jQuery Playground</h3> */
+
+/* Ahora vamos a asegurarnos que todo el contenido de tu página sea adaptable para dispositivos móviles.
+
+Anidemos tu elemento h3 dentro de un elemento div con la clase container-fluid. */
+
+/* <div class="container-fluid"><h3 class="text-primary text-center">jQuery Playground</h3></div> */
+
+/* Ahora crearemos una fila Bootstrap para nuestros elementos en línea.
+
+Crea un elemento div debajo de la etiqueta h3, con la clase row. */
+
+/* <div class="container-fluid">
+  <h3 class="text-primary text-center">jQuery Playground</h3>
+  <div class="row">
+  </div>
+</div> */
+
+/* Ahora que tenemos una fila de Bootstrap, vamos a dividirla en dos columnas para hospedar nuestros elementos.
+
+Crea dos elementos div dentro de tu fila, ambos con la clase col-xs-6. */
+
+/* <div class="container-fluid">
+  <h3 class="text-primary text-center">jQuery Playground</h3>
+  <div class="row">
+    <div class="col-xs-6">
+
+
+    </div>
+    <div class="col-xs-6">
+
+
+    </div>
+
+
+  </div>
+</div> */
+
+/* Bootstrap tiene una clase llamada well que puede crear una sensación visual de profundidad en tus columnas.
+
+Anida un elemento div con la clase well dentro de cada uno de tus elementos col-xs-6 div. */
+
+/* <div class="container-fluid">
+  <h3 class="text-primary text-center">jQuery Playground</h3>
+  <div class="row">
+    <div class="col-xs-6">
+      <div class="well">
+
+      </div>
+    </div>
+    <div class="col-xs-6">
+      <div class="well">
+
+      </div>
+    </div>
+  </div>
+</div> */
+
+/* Anida tres elementos button dentro de cada uno de los elementos div que posean una clase well. */
+
+/* <div class="container-fluid">
+  <h3 class="text-primary text-center">jQuery Playground</h3>
+  <div class="row">
+    <div class="col-xs-6">
+      <div class="well">
+        <button></button>
+        <button></button>
+        <button></button>
+      </div>
+    </div>
+    <div class="col-xs-6">
+      <div class="well">
+        <button></button>
+        <button></button>
+        <button></button>
+      </div> */
+
+/* Aplica las clases btn y btn-default a cada uno de tus elementos button. */
+
+/* <div class="container-fluid">
+  <h3 class="text-primary text-center">jQuery Playground</h3>
+  <div class="row">
+    <div class="col-xs-6">
+      <div class="well">
+        <button class="btn btn-default"></button>
+        <button class="btn btn-default"></button>
+        <button class="btn btn-default"></button>
+      </div>
+    </div>
+    <div class="col-xs-6">
+      <div class="well">
+        <button class="btn btn-default"></button>
+        <button class="btn btn-default"></button>
+        <button class="btn btn-default"></button>
+      </div>
+    </div>
+  </div>
+</div> */
+
+/* Dale a cada uno de tus elementos button la clase target. */
+
+/* <div class="container-fluid">
+  <h3 class="text-primary text-center">jQuery Playground</h3>
+  <div class="row">
+    <div class="col-xs-6">
+      <div class="well">
+        <button class="btn btn-default target"></button>
+        <button class="btn btn-default target"></button>
+        <button class="btn btn-default target"></button>
+      </div>
+    </div>
+    <div class="col-xs-6">
+      <div class="well">
+        <button class="btn btn-default target"></button>
+        <button class="btn btn-default target"></button>
+        <button class="btn btn-default target"></button>
+      </div>
+    </div>
+  </div>
+</div> */
+
+/* Agrega el "id" left-well al elemento de la izquierda con la clase "well". Agrega el "id" right-well al elemento de la derecha con la clase "well". */
+
+/* <div class="container-fluid">
+  <h3 class="text-primary text-center">jQuery Playground</h3>
+  <div class="row">
+    <div class="col-xs-6">
+      <div class="well" id="left-well">
+        <button class="btn btn-default target"></button>
+        <button class="btn btn-default target"></button>
+        <button class="btn btn-default target"></button>
+      </div>
+    </div>
+    <div class="col-xs-6">
+      <div class="well" id="right-well">
+        <button class="btn btn-default target"></button>
+        <button class="btn btn-default target"></button>
+        <button class="btn btn-default target"></button>
+      </div>
+    </div>
+  </div>
+</div> */
+
+/* Sobre tu "left-well", dentro de su elemento div col-xs-6, agrega un elemento h4 con el texto #left-well.
+
+Sobre tu "right-well", dentro de su elemento div col-xs-6 agrega un elemento h4 con el texto #right-well. */
+
+/* <div class="container-fluid">
+  <h3 class="text-primary text-center">jQuery Playground</h3>
+  <div class="row">
+    <div class="col-xs-6">
+      <h4>#left-well</h4>
+      <div class="well" id="left-well">
+        <button class="btn btn-default target"></button>
+        <button class="btn btn-default target"></button>
+        <button class="btn btn-default target"></button>
+      </div>
+    </div>
+    <div class="col-xs-6">
+      <h4>#right-well</h4>
+      <div class="well" id="right-well">
+        <button class="btn btn-default target"></button>
+        <button class="btn btn-default target"></button>
+        <button class="btn btn-default target"></button>
+      </div>
+    </div>
+  </div>
+</div> */
+
+/* Dale a cada uno de tus botones un id único, empezando con target1 y finalizando con target6.
+
+Asegúrate de que target1 a target3 se encuentren en #left-well y target4 a target6 se encuentren en #right-well. */
+
+/* <div class="container-fluid">
+  <h3 class="text-primary text-center">jQuery Playground</h3>
+  <div class="row">
+    <div class="col-xs-6">
+      <h4>#left-well</h4>
+      <div class="well" id="left-well">
+        <button class="btn btn-default target" id="target1"></button>
+        <button class="btn btn-default target" id="target2"></button>
+        <button class="btn btn-default target"id="target3"></button>
+      </div>
+    </div>
+    <div class="col-xs-6">
+      <h4>#right-well</h4>
+      <div class="well" id="right-well">
+        <button class="btn btn-default target" id="target4"></button>
+        <button class="btn btn-default target" id="target5"></button>
+        <button class="btn btn-default target" id="target6"></button>
+      </div>
+    </div>
+  </div>
+</div> */
+
+/* Dale a cada uno de tus elementos button, texto que corresponde a su id. */
+
+/* <div class="container-fluid">
+  <h3 class="text-primary text-center">jQuery Playground</h3>
+  <div class="row">
+    <div class="col-xs-6">
+      <h4>#left-well</h4>
+      <div class="well" id="left-well">
+        <button class="btn btn-default target" id="target1">#target1</button>
+        <button class="btn btn-default target" id="target2">#target2</button>
+        <button class="btn btn-default target" id="target3">#target3</button>
+      </div>
+    </div>
+    <div class="col-xs-6">
+      <h4>#right-well</h4>
+      <div class="well" id="right-well">
+        <button class="btn btn-default target" id="target4">#target4</button>
+        <button class="btn btn-default target" id="target5">#target5</button>
+        <button class="btn btn-default target" id="target6">#target6</button>
+      </div>
+    </div>
+  </div>
+</div> */
+
+/* Agrega un comentario en la parte superior de tu HTML que diga Code below this line should not be changed */
+
+/* <!-- Code below this line should not be changed -->
+<div class="container-fluid">
+  <h3 class="text-primary text-center">jQuery Playground</h3>
+  <div class="row">
+    <div class="col-xs-6">
+      <h4>#left-well</h4>
+      <div class="well" id="left-well">
+        <button class="btn btn-default target" id="target1">#target1</button>
+        <button class="btn btn-default target" id="target2">#target2</button>
+        <button class="btn btn-default target" id="target3">#target3</button>
+      </div>
+    </div>
+    <div class="col-xs-6">
+      <h4>#right-well</h4>
+      <div class="well" id="right-well">
+        <button class="btn btn-default target" id="target4">#target4</button>
+        <button class="btn btn-default target" id="target5">#target5</button>
+        <button class="btn btn-default target" id="target6">#target6</button>
+      </div>
+    </div>
+  </div>
+</div> */
 
 /* ------------------------------------------------------------- */
 
-/* ------------------------------------------------------------- */
-
-/* ------------------------------------------------------------- */
-
-/* ------------------------------------------------------------- */
-
-/* ------------------------------------------------------------- */
-
-/* ------------------------------------------------------------- */
-
-/* ------------------------------------------------------------- */
-
-/* ------------------------------------------------------------- */
-
-/* ------------------------------------------------------------- */
-
-/* ------------------------------------------------------------- */
-
-/* ------------------------------------------------------------- */
-
-/* ------------------------------------------------------------- */
-
-/* ------------------------------------------------------------- */
-
-/* ------------------------------------------------------------- */
-
-/* ------------------------------------------------------------- */
-
-/* ------------------------------------------------------------- */
-
-/* ------------------------------------------------------------- */
-
-/* ------------------------------------------------------------- */
-
-/* ------------------------------------------------------------- */
-
-/* ------------------------------------------------------------- */
-
-/* ------------------------------------------------------------- */
-
-/* ------------------------------------------------------------- */
-
-/* ------------------------------------------------------------- */
