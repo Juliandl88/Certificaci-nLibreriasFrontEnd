d662146ef76b17e181e6dea774a4d775268ae6f3
Julian <julian_delisio@hotmail.com>
2023-01-29 23:49:03 -0300

    jQuery ejercicios

---
+++ jQuery.js
@@ -0,0 +1,109 @@
+
+/* jQuery
+jQuery es una de las librerías de JavaScript más utilizadas en el mundo.
+
+En 2006, cuando se lanzó, los principales navegadores manejaban JavaScript de forma ligeramente diferente. jQuery simplificó el proceso de escribir JavaScript del lado del cliente, y también garantizó que tu código funcionara de la misma manera en todos los navegadores.
+
+En este curso, aprenderás a usar jQuery para seleccionar, eliminar, copiar y modificar diferentes elementos de la página. */
+
+/* Dentro de tu elemento script, agrega este código: $(document).ready(function() { a tu script. Luego ciérralo en la siguiente línea (todavía dentro de tu elemento script) con: });
+Esto es importante porque sin tu document ready function, tu código podría ejecutarse antes de que se procese tu HTML, lo que causaría errores. */
+
+/* <script>
+$(document).ready(function() {});
+</script> */
+
+/* ------------------------------------------------------------- */
+
+/* Hagamos que todos tus elementos button reboten. Simplemente agrega este código dentro de tu función document ready:
+
+$("button").addClass("animated bounce"); */
+
+/* <script>
+  $(document).ready(function() {
+$("button").addClass("animated bounce");
+  });
+</script> */
+
+/* ------------------------------------------------------------- */
+
+/* Primero, apuntemos tus elementos div con la clase well usando el selector $(".well").
+
+Ten en cuenta que, al igual que con las declaraciones CSS, escribes un . antes del nombre de la clase.
+
+Luego usa la función .addClass() de jQuery para agregar las clases animated y shake.
+
+Por ejemplo, puedes hacer que todos los elementos con la clase text-primary se agiten agregando lo siguiente a tu función document ready function:
+
+$(".text-primary").addClass("animated shake"); */
+
+/* <script>
+  $(document).ready(function() {
+    $(".well").addClass("animated shake");
+    $(".text-primary").addClass("animated shake");
+  });
+</script> */
+
+/* ------------------------------------------------------------- */
+
+/* ------------------------------------------------------------- */
+
+/* ------------------------------------------------------------- */
+
+/* ------------------------------------------------------------- */
+
+/* ------------------------------------------------------------- */
+
+/* ------------------------------------------------------------- */
+
+/* ------------------------------------------------------------- */
+
+/* ------------------------------------------------------------- */
+
+/* ------------------------------------------------------------- */
+
+/* ------------------------------------------------------------- */
+
+/* ------------------------------------------------------------- */
+
+/* ------------------------------------------------------------- */
+
+/* ------------------------------------------------------------- */
+
+/* ------------------------------------------------------------- */
+
+/* ------------------------------------------------------------- */
+
+/* ------------------------------------------------------------- */
+
+/* ------------------------------------------------------------- */
+
+/* ------------------------------------------------------------- */
+
+/* ------------------------------------------------------------- */
+
+/* ------------------------------------------------------------- */
+
+/* ------------------------------------------------------------- */
+
+/* ------------------------------------------------------------- */
+
+/* ------------------------------------------------------------- */
+
+/* ------------------------------------------------------------- */
+
+/* ------------------------------------------------------------- */
+
+/* ------------------------------------------------------------- */
+
+/* ------------------------------------------------------------- */
+
+/* ------------------------------------------------------------- */
+
+/* ------------------------------------------------------------- */
+
+/* ------------------------------------------------------------- */
+
+/* ------------------------------------------------------------- */
+
+/* ------------------------------------------------------------- */
