dc8fac95ec987adb297d2e4fa352207c0d35bb6d
Julian <julian_delisio@hotmail.com>
2023-02-07 23:46:46 -0300

    React 34

--- React.js
+++ React.js
@@ -397,6 +397,122 @@ ReactDOM.render(<MyComponent/>, document.getElementById("challenge-node")); */
 
 /* ------------------------------------------------------------- */
 
+/* Pasa "props" a un componente funcional sin estado
+Los desafíos anteriores cubrieron varios casos de creación y composición de elementos de JSX, componentes funcionales y componentes de clase estilo ES6 en React. Con estos cimientos, ha llegado la hora de observar otro patrón de uso muy común en React: props. En React, se pueden pasar props, o propiedades a componentes hijos. Digamos, que tienes un componente App que devuelve un componente hijo llamado Welcome, el cual es un componente funcional sin estado. Puedes pasarle una propiedad llamada user a Welcome escribiendo:
+
+<App>
+  <Welcome user='Mark' />
+</App>
+Puedes utilizar atributos personalizados de HTML creados por ti y soportados por React para ser pasados por props a tu componente. En este caso, la propiedad creada user es pasada como atributo al componente Welcome. Dado que Welcome es un componente funcional sin estado, tiene acceso a este valor de la siguiente manera:
+
+const Welcome = (props) => <h1>Hello, {props.user}!</h1>
+Este valor es llamado props por convención y, cuando se trata de componentes funcionales sin estado, se lo considera como un argumento pasado a una función que retorna JSX. Puedes acceder el valor del argumento en el cuerpo de la función. En los componentes de clase, verás que esto es un poco diferente.
+
+Hay componentes Calendar y CurrentDate en el editor de código. Al prensentar CurrentDate desde el componente Calendar, pasa una propiedad de date asignada a la fecha actual desde el objeto Date de JavaScript. Luego, accede a este prop dentro del componente CurrentDate, mostrando su valor dentro de las etiquetas p. Tenga en cuenta que los valores prop se evalúen como JavaScript, deben estar encerrados dentro de corchetes, por ejemplo date={Date()}. */
+
+/* const CurrentDate = (props) => {
+  return (
+    <div>
+      
+      <p>The current date is: {props.date} </p>
+      
+    </div>
+  );
+};
+
+class Calendar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h3>What date is it?</h3>
+        
+        <CurrentDate date={Date()} />
+        
+      </div>
+    );
+  }
+}; */
+
+/* ------------------------------------------------------------- */
+
+/* Pasa un arreglo como "props"
+El último desafío demostró cómo pasar información desde un componente padre a un componente hijo como props o propiedades. Este desafío busca demostrar cómo se pueden pasar arreglos como props. Para pasar un arreglo a un elemento JSX, debe ser tratado como JavaScript y envolverlo entre llaves.
+
+<ParentComponent>
+  <ChildComponent colors={["green", "blue", "red"]} />
+</ParentComponent>
+El componente hijo entonces tiene acceso a la propiedad del arreglo colors. Los métodos de arreglo, como join() pueden ser usados al acceder a la propiedad. const ChildComponent = (props) => <p>{props.colors.join(', ')}</p> Esto unirá a todos elementos colors del arreglo en una cadena separada por comas y se convertirá en: <p>green, blue, red</p> Más tarde, aprenderemos sobre otros métodos comunes para representar arreglos de datos en React.
+
+Están los componentes List y ToDo en el editor de código. Al renderizar cada List del componente ToDo, pasa una propiedad tasks asignada a un arreglo de tareas pendientes, por ejemplo ["walk dog", "workout"]. Luego, accede a este arreglo de tasks en el componente List, mostrando su valor dentro del elemento p. Usa join(", ") para mostrar el arreglo props.tasks en el elemento p como una lista separada por comas. La lista de hoy debe tener al menos 2 tareas y la de mañana debe tener al menos 3 tareas.
+
+ */
+
+/* const List = (props) => {
+  
+  return <p>{props.tasks.join(', ')}</p>
+  
+};
+
+class ToDo extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>To Do Lists</h1>
+        <h2>Today</h2>
+        
+        <List tasks={["walk dog", "workout", "eat"]}/>
+        <h2>Tomorrow</h2>
+        <List tasks={["bedtime", "shouder", "study"]}/>
+        
+      </div>
+    );
+  }
+}; */
+
+/* ------------------------------------------------------------- */
+
+/* ------------------------------------------------------------- */
+
+/* ------------------------------------------------------------- */
+
+/* ------------------------------------------------------------- */
+
+/* ------------------------------------------------------------- */
+
+/* ------------------------------------------------------------- */
+
+/* ------------------------------------------------------------- */
+
+/* ------------------------------------------------------------- */
+
+/* ------------------------------------------------------------- */
+
+/* ------------------------------------------------------------- */
+
+/* ------------------------------------------------------------- */
+
+/* ------------------------------------------------------------- */
+
+/* ------------------------------------------------------------- */
+
+/* ------------------------------------------------------------- */
+
+/* ------------------------------------------------------------- */
+
+/* ------------------------------------------------------------- */
+
+/* ------------------------------------------------------------- */
+
+/* ------------------------------------------------------------- */
+
+/* ------------------------------------------------------------- */
+
 /* ------------------------------------------------------------- */
 
 /* ------------------------------------------------------------- */
