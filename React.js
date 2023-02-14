e4e8c67c6c11d21f8a29f840a51495d5c0a0193b
Julian <julian_delisio@hotmail.com>
2023-02-13 23:26:48 -0300

    React 59

--- React.js
+++ React.js
@@ -821,19 +821,146 @@ Finalmente, haz clic en el botón para ver la renderizado condicional del compon
 
 Pista: ¡No olvides enlazar la palabra clave this al método en el constructor! */
 
-/* ------------------------------------------------------------- */
+/* class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      visibility: false
+    };
+    // Cambia el código debajo de esta línea
+    this.toggleVisibility = this.toggleVisibility.bind(this);
+
+    // Cambia el código encima de esta línea
+  }
+  // Cambia el código debajo de esta línea
+  toggleVisibility() {
+    this.setState(state => {
+      if (state.visibility === true) {
+         return { visibility: false };
+       } else {
+         return { visibility: true };
+      }
+    });
+  }
+
+
+  // Cambia el código encima de esta línea
+  render() {
+    if (this.state.visibility) {
+      return (
+        <div>
+          <button onClick={this.toggleVisibility}>Click Me</button>
+          <h1>Now you see me!</h1>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <button onClick={this.toggleVisibility}>Click Me</button>
+        </div>
+      );
+    }
+  }
+} */
 
 /* ------------------------------------------------------------- */
 
-/* ------------------------------------------------------------- */
+/* Escribe un contador simple CONTADOR
+Puedes diseñar un componente con un estado más complejo combinando los conceptos cubiertos hasta ahora. Estos incluyen inicializar el state, escribir métodos que establezcan state y asignar manejadores de eventos clic para activar estos métodos.
+
+El componente Counter mantiene un seguimiento de un valor count en el state. Hay dos botones que llaman a métodos increment() y decrement(). Escribe estos métodos para que el valor del contador sea incrementado o disminuyendo por 1 cuando se haga clic en el botón apropiado. También, crea un método reset() para que cuando se haga clic en el botón reset, el contador se establezca a 0.
+
+Nota: Asegúrate de no modificar el classNamede los botones. Además, recuerda agregar en el constructor los enlaces necesarios para los métodos recién creados. */
+
+/* class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+    // Cambia el código debajo de esta línea
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+    this.reset = this.reset.bind(this);
+    // Cambia el código encima de esta línea
+  }
+  // Cambia el código debajo de esta línea
+  increment() {
+    this.setState(state => ({
+      count: state.count + 1
+    }));
+  }
+  decrement() {
+    this.setState(state => ({
+      count: state.count - 1
+    }));
+  }
+  reset() {
+    this.setState(state => ({
+      count: 0
+    }));
+  }
+
+  // Cambia el código encima de esta línea
+  render() {
+    return (
+      <div>
+        <button className='inc' onClick={this.increment}>Increment!</button>
+        <button className='dec' onClick={this.decrement}>Decrement!</button>
+        <button className='reset' onClick={this.reset}>Reset</button>
+        <h1>Current Count: {this.state.count}</h1>
+      </div>
+    );
+  }
+}; */
 
 /* ------------------------------------------------------------- */
 
-/* ------------------------------------------------------------- */
+/* Crea una entrada controlada
+Tu aplicación puede tener interacciones más complejas entre state y la interfaz de usuario renderizada. Por ejemplo, elementos de control de formulario para la entrada de texto, tales como input y textarea, mantienen su propio estado en el DOM como los tipos de usuario. Con React, puedes mover este estado mutable hacia el state de un componente de React. La entrada del usuario se convierte en parte del state de la aplicación, así que React controla el valor de ese campo de entrada. Por lo general, si tienes componentes de React con campos de entrada en los que el usuario puede escribir, será un formulario de entrada controlado.
 
-/* ------------------------------------------------------------- */
+El editor de código tiene el esqueleto de un componente llamado ControlledInput para crear un elemento input controlado. El state del componente ya está inicializado con una propiedad input que contiene una cadena vacía. Este valor representa el texto que un usuario escribe en el campo input.
 
-/* ------------------------------------------------------------- */
+Primero, crea un método llamado handleChange() que tiene un parámetro llamado event. Cuando el método es llamado, este recibe un objeto event que contiene una cadena de texto del elemento input. Puedes acceder a esta cadena con event.target.value dentro del método. Actualiza la propiedad input del state del componente con esta nueva cadena.
+
+En el método render, crea el elemento input encima de la etiqueta h4. Añade un atributo value que es igual a la propiedad input del state del componente. Luego añade un manejador de eventos onChange() establecido al método handleChange().
+
+Cuando escribes en el cuadro de entrada, ese texto es procesado por el método handleChange(), establecido como la propiedad input en el state local, y renderizado como el valor en el cuadro input de la página. El state del componente es la única fuente de verdad con respecto a los datos de entrada.
+
+Por último, pero no menos importante, no olvides añadir los enlaces necesarios en el constructor. */
+
+/* class ControlledInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: ''
+    };
+    // Cambia el código debajo de esta línea
+    this.handleChange = this.handleChange.bind(this);
+
+    // Cambia el código encima de esta línea
+  }
+  // Cambia el código debajo de esta línea
+  handleChange(event) {
+    this.setState(state => ({
+      input: event.target.value
+    }));
+  }
+
+  // Cambia el código encima de esta línea
+  render() {
+    return (
+      <div>
+        
+        <input value={this.state.input} onChange={this.handleChange}></input>
+        
+
+        <h4>Controlled Input:</h4>
+        <p>{this.state.input}</p>
+      </div>
+    );
+  }
+}; */
 
 /* ------------------------------------------------------------- */
 
