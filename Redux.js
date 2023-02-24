eb86f449e0b20f9f7d4a00f934b49af5bf382d11
Julian <julian_delisio@hotmail.com>
2023-02-24 15:12:20 -0300

    ReactRedux 18%

--- Redux.js
+++ Redux.js
@@ -49,25 +49,68 @@ let currentState = store.getState()-*/
 
 /* ---------------------------------------------------------------*/
 
-/* ---------------------------------------------------------------*/
+/* Define una acción Redux
+Dado que Redux es un framework de gestión de estado, la actualización del estado es una de sus tareas principales. En Redux, todas las actualizaciones de estado se activan mediante acciones de envío. Una acción es simplemente un objeto de JavaScript que contiene información sobre un evento de acción que ha ocurrido. El almacén Redux recibe estos objetos de acción, y luego actualiza su estado de acuerdo a ello. A veces una acción Redux también lleva algunos datos. Por ejemplo, la acción lleva un nombre de usuario después de que un usuario inicia sesión. Aunque los datos son opcionales, las acciones deben llevar una propiedad type que especifica el "tipo" de acción que se ha producido.
+
+Piensa en las acciones Redux como mensajeros que entregan información sobre los eventos que ocurren en tu aplicación al almacén Redux. A continuación, el almacén se encarga de actualizar el estado en función de la acción realizada.
+
+Escribir una acción Redux es tan sencillo como declarar un objeto con una propiedad de tipo. Declara un objeto action y dale una propiedad type establecida a la cadena 'LOGIN'.
+
+*/
+
+/* // Define una acción aquí:
+
+const action = {
+  type: "LOGIN"
+}*/
 
 /* ---------------------------------------------------------------*/
 
-/* ---------------------------------------------------------------*/
+/* Define un creador de acción
+Después de crear una acción, el siguiente paso es enviar la acción al almacén Redux para que pueda actualizar su estado. En Redux, se definen creadores de acción para lograr esto. Un creador de acción es simplemente una función de JavaScript que devuelve una acción. En otras palabras, los creadores de acción crean objetos que representan eventos de acción.
+
+Define una función llamada actionCreator() que devuelve el objeto action cuando es llamado.*/
+
+/* const action = {
+  type: 'LOGIN'
+}
+// Define un creador de acción aquí:
+
+const actionCreator = () => action;*/
 
 /* ---------------------------------------------------------------*/
 
-/* ---------------------------------------------------------------*/
+/* Envía un evento de acción
+El método dispatch (enviar) es el que se utiliza para enviar acciones al almacén Redux. Llamar a store.dispatch() y pasar el valor devuelto por un creador de acción envía una acción de regreso al almacén.
+
+Recordemos que los creadores de acción devuelven un objeto con una propiedad de tipo que especifica la acción que se ha producido. Luego, el método envía un objeto de acción al almacén Redux. Basándonos en el ejemplo del desafío anterior, las siguientes líneas son equivalentes, y ambas envían la acción de tipo LOGIN:
+
+store.dispatch(actionCreator());
+store.dispatch({ type: 'LOGIN' });
+El almacén Redux en el editor de código tiene un estado inicializado que es un objeto que contiene una propiedad login actualmente establecida a false. También hay un creador de acción llamado loginAction() que devuelve una acción de tipo LOGIN. Envía la acción LOGIN al almacén Redux llamando al método dispatch, y pasa la acción creada por loginAction().*/
+
+/* const store = Redux.createStore(
+  (state = {login: false}) => state
+);
+
+const loginAction = () => {
+  return {
+    type: 'LOGIN'
+  }
+};
+
+// Despacha la acción aquí:
+
+store.dispatch(loginAction());*/
 
 /* ---------------------------------------------------------------*/
 
-/* ---------------------------------------------------------------*/
+/* Maneja una acción en el almacén
+Después de crear y enviar una acción, el almacén Redux necesita saber cómo responder a esa acción. Este es el trabajo de una función reducer (reductor). Los reductores en Redux son responsables de las modificaciones de estado que tienen lugar en respuesta a las acciones. Un reducer toma state y action como argumentos, y siempre devuelve un nuevo state. Es importante ver que esta es la única función del reductor. No tiene efectos secundarios: nunca llama a un endpoint del API y nunca tiene sorpresas ocultas. El reductor es simplemente una función pura que toma el estado y la acción, y luego devuelve el nuevo estado.
 
-/* ---------------------------------------------------------------*/
+Otro principio clave en Redux es que state es de sólo lectura. En otras palabras, la función reducer debe siempre devolver una nueva copia de state y nunca modificar el estado directamente. Redux no impone la inmutabilidad del estado, sin embargo, tú eres responsable de imponerla en el código de tus funciones reductoras. Practicarás esto en desafíos posteriores.
 
-/* ---------------------------------------------------------------*/
-
-/* ---------------------------------------------------------------*/
+El editor de código tiene el ejemplo anterior, así como el inicio de una función reducer para ti. Rellena el cuerpo de la función reducer para que si recibe una acción de tipo 'LOGIN' devuelva un objeto de estado con login establecido a true. De lo contrario, devuelve el state actual. Ten en cuenta que el state actual y la action enviada se pasan al reductor, por lo que puedes acceder al tipo de la acción directamente con action.type.*/
 
 /* ---------------------------------------------------------------*/
 
